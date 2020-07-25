15f66f8415aa60131abfb371996c5387a5bd2f39
Federico Grandi <fgrandi30@gmail.com>
2020-07-25 10:32:00 +0000

    [auto] Update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -12,7 +12,6 @@ import * as http from 'http'
  * * listmybots
  * * mythicalbots
  * * topgg
- * @typedef {string} ServiceKey
  */
 export type ServiceKey = string
 
@@ -21,7 +20,6 @@ export type ServiceKey = string
  * * vote
  * * like (from listmybots)
  * * unlike (from listmybots)
- * @typedef {string} RichEventType
  */
 export type RichEventType = string
 
@@ -30,7 +28,6 @@ export type RichEventType = string
  * Each key can either have a secret or be `true` to accept *all* requests regardless of authorization.
  *
  * Note: `arcanebotcenter` and `botlistspace` gives you a secret instead of setting one.
- * @typedef {Object.<ServiceKey, string|boolean>} AuthorizationSecrets
  */
 export interface AuthorizationSecrets {
     [key: string]: string | boolean
@@ -38,9 +35,8 @@ export interface AuthorizationSecrets {
 
 /**
  * Options for a hook.
- * @typedef {Object} HookOptions
- * @property {AuthorizationSecrets} [authSecrets] The authorization secrets the hook will use
- * @property {Express} [app] The application to use, will initiate a new application if not defined
+ * @property [authSecrets] - The authorization secrets the hook will use
+ * @property [app] - The application to use, will initiate a new application if not defined
  */
 export interface HookOptions {
     authSecrets?: AuthorizationSecrets
@@ -49,13 +45,8 @@ export interface HookOptions {
 
 /**
  * A generic event object.
- * @typedef {Object} Event
- *
- * These properties need to be synched with RichEvent and ErrorEvent.
- * There is no JSDoc way of extending typdefs that compiles into an extended interface in TS
- * or that is correctly read as an extension by the docs website.
- * @property {ServiceKey} list The service endpoint this event occured in
- * @property {Request} request The request that emitted this event
+ * @property list - The service endpoint this event occured in
+ * @property request - The request that emitted this event
  */
 export interface Event {
     list: ServiceKey
@@ -65,21 +56,17 @@ export interface Event {
 /**
  * An event object that is returned and parsed at the end of an endpoint call.
  * See the README.md of dbothook.js to see which service returns what values.
- * @typedef {Object} RichEvent
- *
- * These properties are from the Event typedef:
- * @property {ServiceKey} list The service endpoint this event occured in
- * @property {Request} request The request that emitted this event
- *
- * @property {Object} data The raw data that the service posted
- * @property {number} timestamp The time when this event was emitted
- * @property {RichEventType} type The type of event
- * @property {boolean} test Whether or not the event was a test sent from the service.
+ * @property list - The service endpoint this event occured in
+ * @property request - The request that emitted this event
+ * @property data - The raw data that the service posted
+ * @property timestamp - The time when this event was emitted
+ * @property type - The type of event
+ * @property test - Whether or not the event was a test sent from the service.
  * Make sure you check this first before processing anything.
- * @property {string} userId The user's ID that emitted the event
- * @property {string} [botId] The bot's ID that the user is calling the event towards
- * @property {string} [username] The user's username
- * @property {string} [discriminator] The user's discriminator (i.e. `#0001`)
+ * @property userId - The user's ID that emitted the event
+ * @property [botId] - The bot's ID that the user is calling the event towards
+ * @property [username] - The user's username
+ * @property [discriminator] - The user's discriminator (i.e. `#0001`)
  */
 export interface RichEvent {
     list: ServiceKey
@@ -96,13 +83,9 @@ export interface RichEvent {
 
 /**
  * A event emitted after an error.
- * @typedef {Object} ErrorEvent
- *
- * These properties are from the Event typedef:
- * @property {ServiceKey} list The service endpoint this event occured in
- * @property {Request} request The request that emitted this event
- *
- * @property {Error} error The error that occurred
+ * @property list - The service endpoint this event occured in
+ * @property request - The request that emitted this event
+ * @property error - The error that occurred
  */
 export interface ErrorEvent {
     list: ServiceKey
@@ -114,33 +97,28 @@ export class DBotHook {
     constructor(options?: HookOptions);
     /**
      * Whether or not endpoints has been added
-     * @type {boolean}
      */
     endpointsAdded: boolean
     /**
      * The authorization secrets the hook is using
-     * @type {AuthorizationSecrets}
      */
     authSecrets: AuthorizationSecrets
     /**
      * The express application being used
-     * @type {Express}
      */
     app: Express
     /**
      * The server created from {@link #listen}
-     * @type {Server}
      */
     server: Server
     /**
      * Loads endpoints into the application
-     * @param {string} [prefix='/'] The prefix to add before the path (which would be the {@link ServiceKey})
+     * @param [prefix = '/'] - The prefix to add before the path (which would be the {@link ServiceKey})
      */
     addEndpoints(prefix?: string): void
     /**
      * Convenience method for app.listen()
-     * @param {...any} args The args that are sent to the application
-     * @see https://expressjs.com/en/4x/api.html#app.listen
+     * @param args - The args that are sent to the application
      */
     listen(...args: any[]): void
 }
